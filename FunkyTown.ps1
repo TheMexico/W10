83231b94fa5ea873f1dfc776ee99b2c8e641f7ac
TheMexico <zamudiom@gmail.com>
2021-02-02 20:33:59 -0500

    Create FunkyTown.ps1

---
+++ FunkyTown.ps1
@@ -0,0 +1,12 @@
+[console]::beep(262,200)
+[console]::beep(262,200) 
+[console]::beep(233,200) 
+[console]::beep(262,200) 
+Start-Sleep -Milliseconds 200
+[console]::beep(196,200) 
+Start-Sleep -Milliseconds 200
+[console]::beep(196,200) 
+[console]::beep(262,200)
+[console]::beep(349,200)
+[console]::beep(330,200) 
+[console]::beep(262,200) 
